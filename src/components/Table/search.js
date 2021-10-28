8604a5ab6cf202e30d6ae029222a4b941974070d
floare <adifloare1969@gmail.com>
2021-10-28 15:58:10 +0300

    [search-page] Add custom search

---
+++ src/components/Table/search.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import { Icon } from '@iconify/react';
+import searchFill from '@iconify/icons-eva/search-fill';
+import trash2Fill from '@iconify/icons-eva/trash-2-fill';
+import roundFilterList from '@iconify/icons-ic/round-filter-list';
+// material
+import { styled } from '@mui/material/styles';
+import {
+    Box,
+    Toolbar,
+    OutlinedInput,
+    InputAdornment
+} from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Toolbar)(({ theme }) => ({
+    height: 96,
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: theme.spacing(0, 1, 0, 3)
+}));
+
+const SearchStyle = styled(OutlinedInput)(({ theme }) => ({
+    width: '50%',
+    transition: theme.transitions.create(['box-shadow', 'width'], {
+        easing: theme.transitions.easing.easeInOut,
+        duration: theme.transitions.duration.shorter
+    }),
+    '&.Mui-focused': { width: '100%', boxShadow: theme.customShadows.z8 },
+    '& fieldset': {
+        borderWidth: `2px !important`,
+        borderColor: `black !important`
+    }
+}));
+
+// ----------------------------------------------------------------------
+
+Search.propTypes = {
+    numSelected: PropTypes.number,
+    filterName: PropTypes.string,
+    onFilterName: PropTypes.func
+};
+
+export default function Search({ filter, onFilter }) {
+    return (
+        <RootStyle>
+            <SearchStyle
+                value={filter}
+                onChange={onFilter}
+                placeholder="Search..."
+                startAdornment={
+                    <InputAdornment position="start">
+                        <Box component={Icon} icon={searchFill} sx={{ color: 'text.disabled' }} />
+                    </InputAdornment>
+                }
+            />
+        </RootStyle>
+    );
+}
