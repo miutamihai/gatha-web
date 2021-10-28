5eeee2ad640acb20b6cefbbc4dcc0821b4feee79
floare <adifloare1969@gmail.com>
2021-10-28 15:58:23 +0300

    [search-page] Add header

---
+++ src/components/Table/header.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import { visuallyHidden } from '@mui/utils';
+import { Box, TableRow, TableCell, TableHead, TableSortLabel } from '@mui/material';
+
+Header.propTypes = {
+    order: PropTypes.oneOf(['asc', 'desc']),
+    orderBy: PropTypes.string,
+    headLabel: PropTypes.array,
+    onRequestSort: PropTypes.func,
+};
+
+export default function Header({
+    order,
+    orderBy,
+    headLabel,
+    onRequestSort,
+}) {
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headLabel.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align="center"
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            hideSortIcon
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box sx={{ ...visuallyHidden }}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
