832e20eee9bcc44982f5d4d95d11a944425abb60
OctaFloare <74454053+OctaFloare@users.noreply.github.com>
2021-10-29 00:07:12 +0300

    [expected-visitor-page] Add visitor page (#5)
    * [expected-visitor-page] Add visitor page
    
    * [expected-visitor-page] Fix code

--- src/_mocks_/user.js
+++ src/_mocks_/user.js
@@ -6,24 +6,8 @@ import { mockImgAvatar } from '../utils/mockImages';
 // ----------------------------------------------------------------------
 
 const users = [...Array(24)].map((_, index) => ({
-  id: faker.datatype.uuid(),
-  avatarUrl: mockImgAvatar(index + 1),
-  name: faker.name.findName(),
-  company: faker.company.companyName(),
-  isVerified: faker.datatype.boolean(),
-  status: sample(['active', 'banned']),
-  role: sample([
-    'Leader',
-    'Hr Manager',
-    'UI Designer',
-    'UX Designer',
-    'UI/UX Designer',
-    'Project Manager',
-    'Backend Developer',
-    'Full Stack Designer',
-    'Front End Developer',
-    'Full Stack Developer'
-  ])
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName()
 }));
 
 export default users;
