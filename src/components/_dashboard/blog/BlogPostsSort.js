34496b64d14146f5d49645bc41623dd9d9059245
floare <adifloare1969@gmail.com>
2021-10-28 16:03:11 +0300

    [search-page] Add styling to menuItem

--- src/components/_dashboard/blog/BlogPostsSort.js
+++ src/components/_dashboard/blog/BlogPostsSort.js
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
 // material
 import { MenuItem, TextField } from '@mui/material';
-
-// ----------------------------------------------------------------------
+import { makeStyles } from "@mui/styles";
 
 BlogPostsSort.propTypes = {
   options: PropTypes.array,
   onSort: PropTypes.func
 };
 
+const useStyles = makeStyles ({
+  sort: {
+    position: 'absolute',
+    top: '135px',
+    left: '500px',
+  },
+  item:{
+    borderRadius: '60%'
+  }
+})
+
 export default function BlogPostsSort({ options, onSort }) {
+  const classes = useStyles()
+  
   return (
-    <TextField select size="small" value="latest" onChange={onSort}>
+    <TextField select size="big" value="latest" onChange={onSort} className={classes.sort}>
       {options.map((option) => (
-        <MenuItem key={option.value} value={option.value}>
+        <MenuItem key={option.value} value={option.value} className={classes.item}>
           {option.label}
         </MenuItem>
       ))}
